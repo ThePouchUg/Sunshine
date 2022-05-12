7a434d1acb98be28e70806f4b4e481ab873c93ab
Aaron Edaku <eaaron25@gmail.com>
2022-05-12 14:03:55 +0300

    Initial commit

--- app/src/main/java/com/inonidakii/sunshine/MainActivity.java
+++ app/src/main/java/com/inonidakii/sunshine/MainActivity.java
@@ -1,9 +1,9 @@
 package com.inonidakii.sunshine;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,11 +16,22 @@ public class MainActivity extends AppCompatActivity {
 
         eWeatherTextView = findViewById(R.id.tv_weather_data);
 
-        String [] thorsNotes = {"Rainy", "Flooded", "Icy", "Frozen", "Muddy", "Slippery"};
+        // TODO (4) Delete the dummy weather data. You will be getting REAL data from the Internet in this lesson.
+        String[] thorsNotes = {"Rainy", "Flooded", "Icy", "Frozen", "Muddy", "Slippery"};
 
-        for (String weather:thorsNotes) {
+        // TODO (3) Delete the for loop that populates the TextView with dummy data
+        for (String weather : thorsNotes) {
             eWeatherTextView.append(weather + "\n");
         }
 
+        // TODO (9) Call loadWeatherData to perform the network request to get the weather
+
     }
+
+    // TODO (8) Create a method that will get the user's preferred location and execute your new AsyncTask and call it loadWeatherData
+
+    // TODO (5) Create a class that extends AsyncTask to perform network requests
+    // TODO (6) Override the doInBackground method to perform your network requests
+    // TODO (7) Override the onPostExecute method to display the results of the network request
+
 }
