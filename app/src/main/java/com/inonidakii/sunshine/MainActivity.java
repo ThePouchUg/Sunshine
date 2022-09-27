1b14b7e7e35329e47940225777bb61bb00bc0987
dakyboy <eaaron25@gmail.com>
2022-09-27 20:50:07 +0300

    Initial commit.

--- app/src/main/java/com/inonidakii/sunshine/MainActivity.java
+++ app/src/main/java/com/inonidakii/sunshine/MainActivity.java
@@ -22,14 +22,29 @@ public class MainActivity extends AppCompatActivity {
     TextView eWeatherTextView;
     EditText eLocationEditText;
 
+    // TODO (6) Add a TextView variable for the error message display
+
+    // TODO (16) Add a ProgressBar variable to show and hide the progress bar
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /*
+         * Using findViewById, we get a reference to our TextView from xml. This allows us to
+         * do things like set the text of the TextView.
+         */
         eWeatherTextView = findViewById(R.id.tv_weather_data);
         eLocationEditText = findViewById(R.id.et_user_location);
 
+
+        // TODO (7) Find the TextView for the error message using findViewById
+
+        // TODO (17) Find the ProgressBar using findViewById
+
+        /* Once all of our views are setup, we can load the weather data. */
+        loadWeatherData();
 
     }
 
@@ -55,13 +70,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void loadWeatherData() {
+        // TODO (20) Call showWeatherDataView before executing the AsyncTask
         String location = SunshinePreferences.getPreferredWeatherLocation(this);
         new GetWeatherDataTask().execute(location);
 
     }
 
+    // TODO (8) Create a method called showWeatherDataView that will hide the error message and show the weather data
+
+    // TODO (9) Create a method called showErrorMessage that will hide the weather data and show the error message
     class GetWeatherDataTask extends AsyncTask<String, Void, String[]> {
 
+        // TODO (18) Within your AsyncTask, override the method onPreExecute and show the loading indicator
 
         @Override
         protected String[] doInBackground(String... params) {
@@ -89,15 +109,19 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(String[] weatherData) {
+            // TODO (19) As soon as the data is finished loading, hide the loading indicator
             if (weatherData != null) {
-                /*Iterate through the array and append the strings to the textview. The reason we add
-                the "\n\n\n" after the String is to give visual separation between seach String in the
-                Later we 'll learn about a better way to display lists of data.
-                * */
+                // TODO (11) If the weather data was not null, make sure the data view is visible
+                /*
+                 * Iterate through the array and append the Strings to the TextView. The reason why we add
+                 * the "\n\n\n" after the String is to give visual separation between each String in the
+                 * TextView. Later, we'll learn about a better way to display lists of data.
+                 */
                 for (String weatherString : weatherData) {
                     eWeatherTextView.append(weatherString + "\n\n\n");
                 }
             }
+            // TODO (10) If the weather data was null, show the error message
         }
     }
 }
